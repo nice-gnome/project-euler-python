f5ca9c0c670732c25777ed32e06005fb31a6a0de
Nice Gnome <nicegnome1@gmail.com>
2023-01-08 11:22:43 -0800

    added divisors helper, problem 12

--- euler-problems/helpers.py
+++ euler-problems/helpers.py
@@ -11,3 +11,11 @@ def is_prime(n):
             return False
         i = i + 6
     return True
+
+def divisors(n):
+    divisors = []
+    for i in range(1,int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            divisors.append(n//i)
+    return divisors
