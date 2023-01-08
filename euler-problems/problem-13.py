60eebbbd4ecb4c5a3e986dc3031e6a32612d7a48
Nice Gnome <nicegnome1@gmail.com>
2023-01-08 12:10:05 -0800

    problem 13

---
+++ euler-problems/problem-13.py
@@ -0,0 +1,13 @@
+"""
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+(Numbers in resources)
+"""
+def first_ten():
+    numbers = open("resources/problem-13-numbers.txt","r")
+    sum = 0
+    for line in numbers:
+        sum += int(line)
+    return str(sum)[:10]
+
+# solution
+print(first_ten())
