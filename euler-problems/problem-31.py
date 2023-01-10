d81ecd1316ed26b332df62b863c6183de66c2292
Nice Gnome <nicegnome1@gmail.com>
2023-01-10 10:06:33 -0800

    problem 31

---
+++ euler-problems/problem-31.py
@@ -0,0 +1,23 @@
+"""
+In the United Kingdom the currency is made up of pound (£) and pence (p). There are eight coins in general circulation:
+
+1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+It is possible to make £2 in the following way:
+
+1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+How many different ways can £2 be made using any number of coins?
+"""
+# N = 200
+# S = [1, 2, 5, 10, 20, 50, 100, 200]
+
+def num_ways(N, S):t
+    if N < 0 or len(S) <= 0:
+        return 0
+    if N == 0:
+        return 1
+    return num_ways(N, S[:-1]) + num_ways(N - S[-1], S)
+
+# solution
+N = 200
+S = [1, 2, 5, 10, 20, 50, 100, 200]
+print(num_ways(N, S))
