06704d85149f0cc421feda132791c8ef0bff1b58
Nice Gnome <nicegnome1@gmail.com>
2023-01-13 10:31:25 -0800

    clean up

--- euler-problems/Problems 51-100/problem-54.py
+++ euler-problems/Problems 51-100/problem-54.py
@@ -95,19 +95,6 @@ def rank_hand(hand):
         return 'one pair'
     return 'high card'
 
-# High Card: Highest value card.
-# One Pair: Two cards of the same value.
-# Two Pairs: Two different pairs.
-# Three of a Kind: Three cards of the same value.
-# Straight: All cards are consecutive values.
-# Flush: All cards of the same suit.
-# Full House: Three of a kind and a pair.
-# Four of a Kind: Four cards of the same value.
-# Straight Flush: All cards are consecutive values of same suit.
-# Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
-# The cards are valued in the order:
-# 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
-
 def resolve_tie(h1, h2, rank):
     # not considering ties: "in each hand there is a clear winner"
     v1 = [c[0] for c in h1]
